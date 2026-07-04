212ae8a9a8db37b5fc4823b08ec7caf85ea70838
Azeem Memon <azeemmemon2003@gmail.com>
2026-07-04 20:16:37 +0500

    Optimized browser to reuse single tab

--- scraper/browser.py
+++ scraper/browser.py
@@ -4,8 +4,10 @@ from playwright.sync_api import sync_playwright
 class Browser:
 
     def __init__(self):
+
         self.playwright = None
         self.browser = None
+        self.page = None
 
     def start(self):
 
@@ -15,17 +17,18 @@ class Browser:
             headless=False
         )
 
+        # Create ONLY ONE tab
+        self.page = self.browser.new_page()
+
     def open(self, url):
 
-        page = self.browser.new_page()
-
-        page.goto(
+        self.page.goto(
             url,
             wait_until="domcontentloaded",
             timeout=60000
         )
 
-        return page
+        return self.page
 
     def stop(self):
 
