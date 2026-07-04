34c4d40317e42e24e5752ed6996747facbf481c2
Azeem Memon <azeemmemon2003@gmail.com>
2026-07-04 23:25:28 +0500

    Added DuckDuckGo search engine

---
+++ search/search.py
@@ -0,0 +1,27 @@
+from duckduckgo_search import DDGS
+
+
+class SearchEngine:
+
+    def search(self, keyword, max_results=20):
+
+        websites = []
+
+        with DDGS() as ddgs:
+
+            results = ddgs.text(
+                keyword,
+                max_results=max_results
+            )
+
+            for result in results:
+
+                url = result.get("href")
+
+                if not url:
+                    continue
+
+                if url not in websites:
+                    websites.append(url)
+
+        return websites
